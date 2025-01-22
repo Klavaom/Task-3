ded33e1846bef6d8a3c871231e80f32375a6c237
Klavaom <6705456@mail.ru>
2025-01-22 20:39:08 +0300

    Update 3 task.py

--- 3 task.py
+++ 3 task.py
@@ -1,21 +1,18 @@
 from os import path
 
 
-def New_file(number):
+def new_file(number):
     number = int(number)*2
-    number = str(number)
-    new_file = open(number + ".txt", "w")
-    new_file.write(number)
-    new_file.close
-    new_file = open(number + ".txt", "r")
-    with open(number + ".txt", "r") as file:
+    with open(f"{number}.txt", "w") as file:
+        file.write(f'{number}')
+    with open(f"{number}.txt", "r") as file:
         open_file = file.read()
     return(open_file)
 
 
-assert New_file(232) == "464"
-assert New_file(70) == "140"
-assert New_file(2) == "4"
+assert new_file(232) == "464"
+assert new_file(70) == "140"
+assert new_file(2) == "4"
 
 print(path.exists("464.txt"))
 print(path.exists("140.txt"))
